3ad61b98c46d26d6ea1fa2158f812a7bf3995097
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-22 15:52:36 +0200

    File

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 
@@ -45,7 +45,8 @@ def number_template(n):
     if n.isdigit():
         return render_template('5-number.html', n=n)
     else:
-        return "Not Found", 404
+        return abort(404, description="Resource not found. Please ensure the\
+path is correct and try again.")
 
 
 if __name__ == "__main__":
