3bb0e9921e2ebe07c6aef6ab3618b17343d09b57
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-24 22:13:40 +0200

    Files

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -18,11 +18,13 @@ def states():
 
 @app.route('/states/<id>')
 def state_Id(id):
-    state = storage.get("State", id)
+    all_states = storage.all("State").values()
+    state = next((s for s in all_states if s.id == id), None)
     if state is None:
         return render_template("404.html"), 404
     cities = sorted(state.cities, key=lambda city: city.name)
     return render_template('9-states.html', state=state, cities=cities)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
